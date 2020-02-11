8584e9fc91d4793a109132c5ebe4f41c8689ec27
leplay <leqcany@gmail.com>
2020-02-11 14:31:58 +0800

    Debug action

--- index.js
+++ index.js
@@ -4,55 +4,13 @@ const yaml = require('js-yaml')
 
 async function run () {
   try {
-    const token = core.getInput('github-token', { required: true })
-    const client = new github.GitHub(token)
-    const labelGlobs = await getLabelGlobs(client)
-    console.log(labelGlobs)
-
-    const labels = []
-    for (const [label, globs] of labelGlobs.entries()) {
-      core.debug(`processing ${label}`)
-    }
+    // const token = core.getInput('github-token', { required: true })
+    // const client = new github.GitHub(token)
+    console.log(123)
   } catch (error) {
     core.error(error)
     core.setFailed(error.message)
   }
-}
-async function getLabelGlobs (client) {
-  const configurationContent = await fetchContent(client)
-  const configObject = yaml.safeLoad(configurationContent)
-  console.log(configObject)
-  return getLabelGlobMapFromObject(configObject)
-}
-async function fetchContent (client) {
-  const response = await client.repos.getContents({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    path: '',
-    ref: github.context.sha
-  })
-  return Buffer.from(response.data.content, 'base64').toString()
-}
-function getLabelGlobMapFromObject (configObject) {
-  const labelGlobs = new Map()
-  for (const label in configObject) {
-    if (typeof configObject[label] === 'string') {
-      labelGlobs.set(label, [configObject[label]])
-    } else if (configObject[label] instanceof Array) {
-      labelGlobs.set(label, configObject[label])
-    } else {
-      throw Error(`found unexpected type for label ${label} (should be string or array of globs)`)
-    }
-  }
-  return labelGlobs
-}
-async function addLabels (client, prNumber, labels) {
-  await client.issues.addLabels({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    issue_number: prNumber,
-    labels: labels
-  })
 }
 
 run()
