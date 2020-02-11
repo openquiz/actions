7f161e7716e7fb0ca1131cfbedcf76400693300d
leplay <leqcany@gmail.com>
2020-02-11 14:44:14 +0800

    Debug action

--- index.js
+++ index.js
@@ -5,12 +5,37 @@ const yaml = require('js-yaml')
 async function run () {
   try {
     const token = core.getInput('github-token', { required: true })
-    // const client = new github.GitHub(token)
+    const client = new github.GitHub(token)
     console.log(123)
+
+    const issueNumber = getIssueNumber()
+    console.log(issueNumber)
+    if (issueNumber == null) {
+      throw new Error('No Issue Provided')
+    }
+
+    const { data } = await client.issues.get({
+      ...getRepo(),
+      issueNumber
+    })
+    console.log(data)
   } catch (error) {
     core.error(error)
     core.setFailed(error.message)
   }
+}
+
+function getIssueNumber () {
+  const issue = github.context.payload.issue
+  if (!issue) {
+    return undefined
+  }
+  return issue.number
+}
+
+function getRepo () {
+  const repo = github.context.repo
+  return repo
 }
 
 run()
